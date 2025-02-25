490fae24e3467a97d39b2bd177b480026030002e
Innocentadika <innocentadika18@gmail.com>
2025-02-25 20:58:36 +0300

    creating addBook method with post

--- client/app/src/App.jsx
+++ client/app/src/App.jsx
@@ -2,42 +2,82 @@ import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-const [books, setBooks] = useState([]);
+  const [books, setBooks] = useState([]);
+  const [title, setTitle] = useState('');
+  const [releaseYear, setReleaseYear] = useState(0);
 
-useEffect(() => {
-  fetchBooks();
-}, []);
+  const handleTitle = (e) => {
+    setTitle(e.target.value);
+  };
 
-const fetchBooks = async () => {
-  try {
-    const response = await fetch("http://127.0.0.1:8000/api/books/");
-    const data = await response.json();
-    setBooks(data);
-    
-  } catch (error) {
-    console.log(error);
-  
+  const handleReleaseYear = (e) => {
+    setReleaseYear(e.target.value);
+  };
+
+  const addBook = async () => {
+    const bookData = {
+      title,
+      release_year: releaseYear,
+    };
+
+    try {
+      const response = await fetch("http://127.0.0.1:8000/api/books/create/", {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(bookData),
+      });
+      const data = await response.json();
+      console.log(data);
+    } catch (err) {
+      console.log(err);
+    }
   }
-}
+
+  useEffect(() => {
+    fetchBooks();
+  }, []);
+
+  const fetchBooks = async () => {
+    try {
+      const response = await fetch("http://127.0.0.1:8000/api/books/");
+      const data = await response.json();
+      setBooks(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   return (
-    <>
+    <div>
       <div className="">
         <h1>Books</h1>
 
         <div className="">
-          <input type="text" placeholder='Book title'/>
-          <input type="number" placeholder='Release date'/>
-          <button type="submit">Add Book</button>
+          <input 
+            type="text" 
+            value={title} 
+            onChange={handleTitle} 
+            placeholder='Book title' 
+          />
+          <input 
+            type="number" 
+            value={releaseYear} 
+            onChange={handleReleaseYear} 
+            placeholder='Release year' 
+          />
+          <button type="button" onClick={addBook}>Add Book</button>
         </div>
-        {books.map((books, id) => (
+
+        {books.map((book, id) => (
           <div key={id}>
-            <p>{books.title}</p>
-            <p>{books.release_year}</p>
+            <p>Title: {book.title}</p>
+            <p>Release year: {book.release_year}</p>
           </div>
         ))}
       </div>
-    </>
+    </div>
   )
 }
 
